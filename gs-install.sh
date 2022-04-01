967bbce93a5347cb736154086d357b6090660924
Michael Stanclift <mx@vmstan.com>
2022-04-01 12:05:20 -0500

    sudo do many things

--- gs-install.sh
+++ gs-install.sh
@@ -244,8 +244,8 @@ else
             echo -e "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞"
             if [ "$GS_DEV" != "" ]; then
                 sudo git clone -b ${GS_DEV} https://github.com/vmstan/gravity-sync.git /etc/gravity-sync/.gs
-                touch /etc/gravity-sync/.gs/dev
-                echo -e "BRANCH='origin/$GS_DEV'" >> /etc/gravity-sync/.gs/dev
+                sudo touch /etc/gravity-sync/.gs/dev
+                sudo echo -e "BRANCH='origin/$GS_DEV'" >> /etc/gravity-sync/.gs/dev
             else
                 sudo git clone https://github.com/vmstan/gravity-sync.git /etc/gravity-sync/.gs
             fi
