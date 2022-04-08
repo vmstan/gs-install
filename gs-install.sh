2c51b9a138b5b3fb10cb47fc68cc688c925afcb0
Michael Stanclift <mx@vmstan.com>
2022-04-08 18:37:39 -0500

    auto install

--- gs-install.sh
+++ gs-install.sh
@@ -7,6 +7,26 @@
 # This code will be called from a curl call via installation instructions
 
 set -e
+export PATH+=':/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+
+if hash apt-get; then
+    OS_PKG="debian"
+    OS_UPDATE="apt-get update"
+    OS_INSTALL="apt-get install -y"
+elif hash dnf; then
+    OS_PKG="redhat"
+    OS_UPDATE="dnf check-update"
+    OS_INSTALL="dnf install"
+elif hash tdnf; then
+    OS_PKG="photon"
+    OS_INSTALL="tdnf update"
+    OS_INSTALL="tdnf install"
+elif hash yum; then
+    OS_PKG="oldhat"
+elif hash apk; then
+    OS_PKG="alpine"
+fi
+
 
 # Script Colors
 RED='\033[0;91m'
@@ -34,11 +54,24 @@ PHFAILCOUNT="0"
 CURRENTUSER=$(whoami)
 
 # Header
-echo -e "${LOGO} ${BOLD}Gravity Sync by @vmstan - Installation Script${NC}"
-echo -e "${INFO} Validating user permissions"
+echo -e "${LOGO} ${BOLD}Gravity Sync Installation Script${NC}"
+
+echo -e "${INFO} Validating User Permissions"
 if [ ! "$EUID" -ne 0 ]; then
     echo -e "${GOOD} ${CURRENTUSER} is root"
     LOCALADMIN="root"
+
+    if ! hash sudo 2>/dev/null; then
+        if [ "${OS_PKG}" != "oldhat" ] || [ "${OS_PKG}" != "alpine" ]; then
+            echo -e "${INFO} Installing Sudo"
+            ${OS_INSTALL} sudo
+        else
+            echo -e "${FAIL} Sudo utility not detected"
+            CROSSCOUNT=$((CROSSCOUNT+1))
+        fi
+    else
+        echo -e "${GOOD} Sudo utility detected"
+    fi
 else
     if hash sudo 2>/dev/null; then
         echo -e "${GOOD} Sudo utility detected"
@@ -63,13 +96,26 @@ else
      fi
 fi
 
-echo -e "${INFO} Scanning for Required Components"
+if [ "${LOCALADMIN}" == "nosudo" ]
+    echo -e "${FAIL} Sudo utility cannot be used by the current user"
+    echo -e "  You will need to manually compensate for this error"
+    echo -e "  Installation cannot continue at this time"
+    echo -e "${INFO} Exiting Gravity Sync Installer"
+    exit
+fi
+
+if [ "${OS_PKG}" != "oldhat" ] || [ "${OS_PKG}" != "alpine" ]; then
+    echo -e "${INFO} Attempting Install of Required Components"
+    sudo ${OS_INSTALL} git rsync ssh
+fi
+
+echo -e "${INFO} Validating Install of Required Components"
 # Check OpenSSH
 if hash ssh 2>/dev/null
 then
     echo -e "${GOOD} SSH has been detected"
 else
-    echo -e "${FAIL} OpenSSH not detected on this system"
+    echo -e "${FAIL} SSH not detected on this system"
     echo -e "${WARN} This is required to run commands to your remote Pi-hole"
     CROSSCOUNT=$((CROSSCOUNT+1))
 fi
@@ -81,16 +127,6 @@ then
 else
     echo -e "${FAIL} RSYNC not detected on this system"
     echo -e "${WARN} This is required to transfer data to/from your remote Pi-hole"
-    CROSSCOUNT=$((CROSSCOUNT+1))
-fi
-
-# Check Sudo
-if hash sudo 2>/dev/null
-then
-    echo -e "${GOOD} SUDO has been detected"
-else
-    echo -e "${FAIL} SUDO not detected on this system"
-    echo -e "${WARN} This is required to properly set permissions on your Pi-hole(s)"
     CROSSCOUNT=$((CROSSCOUNT+1))
 fi
 
@@ -123,66 +159,32 @@ if [ "$GS_DOCKER" != "1" ]; then
     then
         echo -e "${GOOD} Local installation of Pi-hole has been detected"
     else
-        echo -e "${WARN} Standard Pi-hole installation is not detected"
-        echo -e "${INF1} Attempting To Compensate"
         if hash docker 2>/dev/null
         then
             echo -e "${GOOD} Docker installation has been detected"
-            
-            if [ "$LOCALADMIN" == "sudo" ]
-            then
-                FTLCHECK=$(sudo docker container ls | grep 'pihole/pihole')
-            elif [ "$LOCALADMIN" == "nosudo" ]
-            then
-                echo -e "${WARN} Unable to detect running Docker containers"
-                # CROSSCOUNT=$((CROSSCOUNT+1))
-                PHFAILCOUNT=$((PHFAILCOUNT+1))
-            else
-                FTLCHECK=$(docker container ls | grep 'pihole/pihole')
-            fi
-            
-            if [ "$LOCALADMIN" != "nosudo" ]
-            then
+            FTLCHECK=$(sudo docker container ls | grep 'pihole/pihole')
                 if [ "$FTLCHECK" != "" ]
                 then
-                    echo -e "${GOOD} Running Docker container of Pi-hole has been detected"
+                    echo -e "${GOOD} Docker container of Pi-hole has been detected"
                 else
                     echo -e "${WARN} There is no Docker container of Pi-hole running"
-                    # CROSSCOUNT=$((CROSSCOUNT+1))
                     PHFAILCOUNT=$((PHFAILCOUNT+1))
                 fi
             fi
         elif hash podman 2>/dev/null
         then
             echo -e "${GOOD} Podman installation has been detected"
-            
-            if [ "$LOCALADMIN" == "sudo" ]
-            then
-                FTLCHECK=$(sudo podman container ls | grep 'pihole/pihole')
-            elif [ "$LOCALADMIN" == "nosudo" ]
-            then
-                echo -e "${WARN} Unable to detect running Podman containers"
-                # CROSSCOUNT=$((CROSSCOUNT+1))
-                PHFAILCOUNT=$((PHFAILCOUNT+1))
-            else
-                FTLCHECK=$(podman container ls | grep 'pihole/pihole')
-            fi
-            
-            if [ "$LOCALADMIN" != "nosudo" ]
-            then
+            FTLCHECK=$(sudo podman container ls | grep 'pihole/pihole')
                 if [ "$FTLCHECK" != "" ]
                 then
-                    echo -e "${GOOD} Running Podman container of Pi-hole has been detected"
-        else
+                    echo -e "${GOOD} Podman container of Pi-hole has been detected"
+                else
                     echo -e "${WARN} There is no Podman container of Pi-hole running"
-                    # CROSSCOUNT=$((CROSSCOUNT+1))
                     PHFAILCOUNT=$((PHFAILCOUNT+1))
                 fi
             fi
         else
-            # echo -e "${FAIL} No Local Pi-hole Install Detected"
-            echo -e "${WARN} No containerized Pi-hole alternatives are detected"
-            # CROSSCOUNT=$((CROSSCOUNT+1))
+            echo -e "${FAIL} No local Pi-hole install detected"
             PHFAILCOUNT=$((PHFAILCOUNT+1))
         fi
     fi
@@ -200,8 +202,8 @@ if [ "$CROSSCOUNT" != "0" ]
 then
     echo -e "${INFO} Status Report"
     echo -e "${FAIL} ${RED}${CROSSCOUNT} critical issue(s) prevent successful deployment${NC}"
-    echo -e "${WARN} Please compensate for the failures and re-execute"
-    echo -e "${INF1} Installation is now exiting making without changes"
+    echo -e "  Please manually compensate for the failures and re-execute"
+    echo -e "${INFO} Exiting Gravity Sync Installer"
 else
     echo -e "${INFO} Executing Gravity Sync Deployment"
     
@@ -216,9 +218,9 @@ else
     if [ "$GS" == "prep" ]
     then
         echo -e "${GOOD} This system has been validated as ready to run Gravity Sync"
-        echo -e "${WARN} Execute again here or on another system without 'GS=prep'"
-        echo -e "${NEED} https://github.com/vmstan/gravity-sync/wiki for questions"
-        echo -e "${INFO} Preperation Complete"
+        echo -e "  Execute again here or on another system without 'GS=prep'"
+        echo -e "  https://github.com/vmstan/gravity-sync/wiki for questions"
+        echo -e "${INFO} Gravity Sync Preperation Complete"
     else
         echo -e "${STAT} Creating Gravity Sync Directories"
             if [ -d /etc/gravity-sync/.gs ]; then
@@ -251,9 +253,9 @@ else
         if [ ! -f /etc/gravity-sync/gravity-sync.conf ]; then 
             gravity-sync configure <&1
         else
-            echo -e "${GOOD} Existing gravity-sync.conf has been detected"
-            echo -e "${WARN} Execute 'gravity-sync config' to replace it"
-            echo -e "${WARN} Use 'gravity-sync update' in the future as an alternative"
+            echo -e "${WARN} Existing gravity-sync.conf has been detected"
+            echo -e "  Execute ${YELLOW}gravity-sync config${NC} to replace it"
+            echo -e "  Use ${YELLOW}gravity-sync update${NC} in the future as an alternative"
             echo -e "${GOOD} Upgrade Complete"
             echo -e "${INFO} Installation Exiting"
         fi
